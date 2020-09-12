02ce35ee1e32a1028c527fe636642a1381a6b148
sonia.cabrera <sonia.cabrera@mh.gob.sv>
2020-09-12 12:19:24 -0600

    otro ups

--- src/main/resources/static/js/services.js
+++ src/main/resources/static/js/services.js
@@ -5,7 +5,7 @@ app.controller('InventarioController', ['$scope', '$http',
     function ($scope, $http) {
         $scope.getInventario= function () {
             $http.get('/inventario').success(function (data) {
-                $scope.inventario = data;
+                $scope.inventarios = data;
             });
         }
 
